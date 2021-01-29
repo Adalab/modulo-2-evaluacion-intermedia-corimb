abc72c1df90db800d8052774df39ef4b2dce6061
Corina <cborcoci@gmail.com>
2021-01-29 16:28:31 +0100

    Add gyphs and change styles

--- js/main.js
+++ js/main.js
@@ -8,10 +8,12 @@
 // Cuando la jugadora no introduzca un número válido y pulse en Prueba: El número debe estar entre 1 y 100.
 
 const button = document.querySelector('.js-btn');
-const replay = document.querySelector('.hidden');
+const replay = document.querySelector('.try-again-btn');
 const track = document.querySelector('.js-track');
 const attempts = document.querySelector('.js-attempts');
 const inputValue = document.querySelector('.js-number');
+const gyphOne = document.querySelector('.giphy-embed-one');
+const gyphTwo = document.querySelector('.giphy-embed-two');
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
@@ -34,8 +36,11 @@ function updateTrack() {
       } else if (value < randomNumber) {
         feedback = 'Pista: Demasiado bajo';
       } else if (value === randomNumber) {
-        feedback = 'Has ganado campeona';
+        feedback = '¡Has ganado!';
+        gyphGenerator();
         replay.classList.remove('hidden');
+        gyphTwo.classList.add('hidden');
+        gyphOne.classList.add('hidden');
       }
     } else {
       feedback = 'Pista: El número debe estar entre 1 y 100';
@@ -56,6 +61,12 @@ let attemptsTrying = 0;
 function updateCounter() {
   attemptsTrying = attemptsTrying + 1;
   attempts.innerHTML = attemptsTrying;
+  if (attemptsTrying > 2){
+    gyphOne.classList.remove('hidden');
+  } if (attemptsTrying > 4){
+    gyphOne.classList.add('hidden');
+    gyphTwo.classList.remove('hidden');
+  }
 }
 
 function reload() {
@@ -75,5 +86,23 @@ button.addEventListener('click', updateTrack);
 replay.addEventListener('click', reload);
 inputValue.addEventListener('keydown', handleSubmitEnter);
 
+
 // * ponemos el prevent default en el evento al pulsar la tecla Enter para que evite enviar el formulario puesto que está relacionado con el valor del input. Lo que hacemos en realidad es evitar el submit del form.
 // Si lo ponemos en la función de updateTrack solo se ejecuta una vez y no tiene en cuenta la tecla Enter que es un evento que sucede después.
+
+import * as CONFIG  from '../config.js';
+
+function gyphGenerator () {
+  fetch(`http://api.giphy.com/v1/gifs/search?q=win&api_key=${CONFIG.default.API_KEY}`)
+  .then(response => response.json())
+    .then(json => {
+      json.data
+        .map(gif => gif.images.fixed_height.url)
+        .forEach(url => {
+          let img = document.createElement('img')
+          img.src = url
+          document.body.appendChild(img)
+        })
+    })
+    .catch(error => document.body.appendChild = error)
+}
